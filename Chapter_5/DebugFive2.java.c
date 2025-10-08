5560b4793568e0ae73a9593665c0ffc52c595c19
EdmarBulawinJr-UCM <edmarbulawin185@gmail.com>
2025-10-08 16:21:05 +0800

    Update DebugFive2.java.c

--- Chapter_5/DebugFive2.java.c
+++ Chapter_5/DebugFive2.java.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int num = 10;     // Simulated user input
-    int num2 = 2;     // Simulated user input
+    int num = 10;     
+    int num2 = 2;    
 
     if ((num2 != 0 && num % num2 == 0) || (num != 0 && num2 % num == 0)) {
         printf("One of these numbers is\n");
@@ -14,3 +14,4 @@ int main() {
 
     return 0;
 }
+
